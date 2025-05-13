ecdf6b3e3fc3defd0addfa6b2980fbb5f3d505af
puliv <pverapavez@gmail.com>
2025-05-12 20:30:36 -0400

    primer commit

---
+++ src/test/java/com/example/edutech/EdutechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
